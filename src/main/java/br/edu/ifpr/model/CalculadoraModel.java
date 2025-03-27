9cb2c78a564e940f1017838ca77aa5613269570d
Rafael de Sá <rafaeldesa170622@gmail.com>
2025-03-26 23:40:19 -0300

    🎉 init: starting project

---
+++ src/main/java/br/edu/ifpr/model/CalculadoraModel.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package br.edu.ifpr.model;
+
+/**
+ *
+ * @author Aluno
+ */
+public class CalculadoraModel {
+
+    public void multiplicacao(double a, double b) {
+
+    }
+
+    public boolean multiplicacaoValidation(double b) {
+        //validar segundo numero 
+        return false;
+    }
+}
---
+++ src/main/java/br/edu/ifpr/view/CalculadoraView.java
@@ -0,0 +1,123 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package br.edu.ifpr.view;
+
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import javax.swing.BoxLayout;
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JOptionPane;
+import javax.swing.JTextField;
+
+/**
+ *
+ * @author efbaro
+ */
+public class CalculadoraView extends JFrame {
+
+    private JTextField campoNumero1;
+    private JTextField campoNumero2;
+    private JTextField campoResultado;
+    private JButton botaoSomar;
+    private JButton botaoSubtrair;
+    private JButton botaoMultiplicar;
+    private JButton botaoDividir;
+
+    public CalculadoraView() {
+
+        setTitle("Calculadora Monolítica");
+        setSize(400, 300);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setLayout(new BoxLayout(getContentPane(), BoxLayout.Y_AXIS));
+
+        campoNumero1 = new JTextField(10);
+        campoNumero2 = new JTextField(10);
+        campoResultado = new JTextField(10);
+        campoResultado.setEditable(false); // Campo de resultado não pode ser editado
+
+        botaoSomar = new JButton("Somar");
+        botaoSubtrair = new JButton("Subtrair");
+        botaoMultiplicar = new JButton("Multiplicar");
+        botaoDividir = new JButton("Dividir");
+
+        add(new JLabel("Numero 1:"));
+        add(campoNumero1);
+        add(new JLabel("Numero 2:"));
+        add(campoNumero2);
+        add(new JLabel("Resultado:"));
+        add(campoResultado);
+        add(botaoSomar);
+        add(botaoSubtrair);
+        add(botaoMultiplicar);
+        add(botaoDividir);
+
+        // Ação do botão "Somar"
+        botaoSomar.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                try {
+                    double num1 = Double.parseDouble(campoNumero1.getText());
+                    double num2 = Double.parseDouble(campoNumero2.getText());
+                    double resultado = num1 + num2;
+                    campoResultado.setText(String.valueOf(resultado));
+                } catch (NumberFormatException ex) {
+                    JOptionPane.showMessageDialog(null, "Erro: Entrada inválida!");
+                }
+            }
+        });
+
+        // Ação do botão "Subtrair"
+        botaoSubtrair.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                try {
+                    double num1 = Double.parseDouble(campoNumero1.getText());
+                    double num2 = Double.parseDouble(campoNumero2.getText());
+                    double resultado = num1 - num2;
+                    campoResultado.setText(String.valueOf(resultado));
+                } catch (NumberFormatException ex) {
+                    JOptionPane.showMessageDialog(null, "Erro: Entrada inválida!");
+                }
+            }
+        });
+
+        // Ação do botão "Multiplicar"
+        botaoMultiplicar.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                try {
+                    double num1 = Double.parseDouble(campoNumero1.getText());
+                    double num2 = Double.parseDouble(campoNumero2.getText());
+                    double resultado = num1 * num2;
+                    campoResultado.setText(String.valueOf(resultado));
+                } catch (NumberFormatException ex) {
+                    JOptionPane.showMessageDialog(null, "Erro: Entrada inválida!");
+                }
+            }
+        });
+
+        // Ação do botão "Dividir"
+        botaoDividir.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                try {
+                    double num1 = Double.parseDouble(campoNumero1.getText());
+                    double num2 = Double.parseDouble(campoNumero2.getText());
+                    if (num2 != 0) {
+                        double resultado = num1 / num2;
+                        campoResultado.setText(String.valueOf(resultado));
+                    } else {
+                        JOptionPane.showMessageDialog(null, "Erro: Divisão por zero!");
+                    }
+                } catch (NumberFormatException ex) {
+                    JOptionPane.showMessageDialog(null, "Erro: Entrada inválida!");
+                }
+            }
+        });
+    }
+
+}
